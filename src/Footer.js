3cd3e99d197d66fff3fa688a6f7adf1c666b6e13
chelseyrhianne <87962835+chelseyrhianne@users.noreply.github.com>
2022-03-08 18:33:01 +1300

    Added base components

---
+++ src/Footer.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./Footer.css";
+
+export default function Footer() {
+  return (
+    <div className="Footer">
+      Challenge by{" "}
+      <a
+        href="https://www.frontendmentor.io?ref=challenge"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Frontend Mentor
+      </a>
+      . Coded by{" "}
+      <a
+        href="https://github.com/chelseyrhianne/react-github-user-search-app"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Chelsey Watson
+      </a>
+      .
+    </div>
+  );
+}
---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Header.css";
+
+export default function Header() {
+  return (
+    <div className="Header">
+      <h1>DevFinder</h1>
+      <p>Dark Light</p>
+    </div>
+  );
+}
